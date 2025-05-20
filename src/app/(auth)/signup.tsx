402bca456b885dba03f6d6892647b2d7dc279bef
An Nguyen <anndb1903@gmail.com>
2025-05-20 23:40:55 +0700

    design sign up page

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,3 +1,5 @@
+import ShareButton from "@/components/button/share.button";
+import { APP_COLOR } from "@/utils/constant";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const styles = StyleSheet.create({
@@ -12,11 +14,10 @@ const styles = StyleSheet.create({
     gap: 10
   },
   text: {
-    fontSize: 20,
-    fontWeight: "600"
+    fontSize: 18
   },
   input: {
-    borderColor: "green",
+    borderColor: "#d0d0d0",
     borderWidth: 1,
     paddingHorizontal: 7,
     paddingVertical: 10,
@@ -27,7 +28,7 @@ const styles = StyleSheet.create({
 const SignUpPage = () => {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={{ padding: 5 }}>
         <Text>Đăng ký tài khoản</Text>
       </View>
       <View style={styles.inputGroup}>
@@ -36,12 +37,28 @@ const SignUpPage = () => {
       </View>
       <View style={styles.inputGroup}>
         <Text style={styles.text}>Email</Text>
-        <TextInput style={styles.input} />
+        <TextInput keyboardType="email-address" style={styles.input} />
       </View>
       <View style={styles.inputGroup}>
         <Text style={styles.text}>Password</Text>
         <TextInput style={styles.input} />
       </View>
+      <ShareButton
+        title="ĐĂNG KÝ"
+        onPress={() => alert("me")}
+        textStyle={{ color: "#fff", paddingVertical: 5 }}
+        btnStyle={{
+          justifyContent: "center",
+          borderRadius: 30,
+          marginHorizontal: 35,
+          paddingVertical: 15,
+          backgroundColor: APP_COLOR.ORANGE,
+          borderWidth: 1,
+          borderColor: "#505050",
+          width: "82%"
+        }}
+        pressStyle={{ alignSelf: "stretch" }}
+      />
     </View>
   );
 };
