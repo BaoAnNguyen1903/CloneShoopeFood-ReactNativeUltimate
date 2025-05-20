74b96ad303d03de4077a604db3493cb7e4724af0
An Nguyen <anndb1903@gmail.com>
2025-05-20 22:46:03 +0700

    welcome page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,124 +1,135 @@
 import { FontAwesome5 } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import ShareButton from "../components/button/share.button";
 import { APP_COLOR } from "../utils/constant";
 
 const WelcomePage = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.welcomeText}>
-        <Text style={styles.heading}>Welcome to</Text>
-        <Text style={styles.body}>Antin Food</Text>
-        <Text style={styles.footer}>
-          Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
-        </Text>
-      </View>
-      <View style={styles.welcomeBtn}>
-        <View
-          style={{
-            borderBottomWidth: 1,
-            borderBottomColor: "red",
-            marginHorizontal: 50
-          }}
-        >
-          <Text
-            style={{
-              padding: 10,
-              textAlign: "center",
-              backgroundColor: "white",
-              alignSelf: "center",
-              position: "relative",
-              top: 20
-            }}
-          >
-            Đăng nhập với
-          </Text>
-        </View>
+    <ImageBackground
+      style={{ flex: 1 }}
+      source={require("@/assets/auth/welcome-back.png")}
+    >
+      <LinearGradient
+        style={{ flex: 1 }}
+        colors={["transparent", "#191B2F"]}
+        locations={[0.2, 0.8]}
+      >
+        <View style={styles.container}>
+          <View style={styles.welcomeText}>
+            <Text style={styles.heading}>Welcome to</Text>
+            <Text style={styles.body}>Antin Food</Text>
+            <Text style={styles.footer}>
+              Nền tảng giao đồ ăn trực tuyến hàng đầu Việt Nam
+            </Text>
+          </View>
+          <View style={styles.welcomeBtn}>
+            <View
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: "red",
+                marginHorizontal: 50
+              }}
+            >
+              <Text
+                style={{
+                  padding: 10,
+                  textAlign: "center",
+                  backgroundColor: "white",
+                  alignSelf: "center",
+                  position: "relative",
+                  top: 20
+                }}
+              >
+                Đăng nhập với
+              </Text>
+            </View>
 
-        <View>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-              gap: 30
-            }}
-          >
-            {/* <View style={styles.btnContent}>
+            <View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  gap: 30
+                }}
+              >
+                {/* <View style={styles.btnContent}>
               <Text style={styles.btnText}>Facebook</Text>
             </View> */}
-            <ShareButton
-              title="faceBook"
-              onPress={() => alert("me")}
-              textStyle={{ textTransform: "uppercase" }}
-              btnStyle={{
-                justifyContent: "center",
-                borderRadius: 30,
-                backgroundColor: "#fff"
-              }}
-              icons={<FontAwesome5 name="facebook" size={30} color={"black"} />}
-            />
+                <ShareButton
+                  title="faceBook"
+                  onPress={() => alert("me")}
+                  textStyle={{ textTransform: "uppercase" }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    backgroundColor: "#fff"
+                  }}
+                  icons={
+                    <FontAwesome5 name="facebook" size={30} color={"black"} />
+                  }
+                />
 
-            <ShareButton
-              title="google"
-              onPress={() => alert("me")}
-              textStyle={{ textTransform: "uppercase" }}
-              btnStyle={{
-                justifyContent: "center",
-                borderRadius: 30,
-                paddingHorizontal: 20,
-                backgroundColor: "#fff"
-              }}
-              icons={<FontAwesome5 name="google" size={30} color={"black"} />}
-            />
-          </View>
+                <ShareButton
+                  title="google"
+                  onPress={() => alert("me")}
+                  textStyle={{ textTransform: "uppercase" }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    paddingHorizontal: 20,
+                    backgroundColor: "#fff"
+                  }}
+                  icons={
+                    <FontAwesome5 name="google" size={30} color={"black"} />
+                  }
+                />
+              </View>
 
-          <View style={{ paddingTop: 20 }}>
-            <ShareButton
-              title="Đăng nhập với email"
-              onPress={() => alert("me")}
-              textStyle={{ color: "#fff", paddingVertical: 5 }}
-              btnStyle={{
-                justifyContent: "center",
-                borderRadius: 30,
-                marginHorizontal: 35,
-                paddingVertical: 10,
-                backgroundColor: "#2c2c2c",
-                borderWidth: 1,
-                borderColor: "red",
-                width: "82%"
-              }}
-              pressStyle={{ alignSelf: "stretch" }}
-            />
+              <View style={{ paddingTop: 20 }}>
+                <ShareButton
+                  title="Đăng nhập với email"
+                  onPress={() => alert("me")}
+                  textStyle={{ color: "#fff", paddingVertical: 5 }}
+                  btnStyle={{
+                    justifyContent: "center",
+                    borderRadius: 30,
+                    marginHorizontal: 35,
+                    paddingVertical: 10,
+                    backgroundColor: "#2c2c2c",
+                    borderWidth: 1,
+                    borderColor: "#ccc",
+                    width: "82%"
+                  }}
+                  pressStyle={{ alignSelf: "stretch" }}
+                />
+              </View>
+            </View>
+            <View>
+              <Text style={{ textAlign: "center", color: "#ccc" }}>
+                Chưa có tài khoản? Đăng ký.
+              </Text>
+            </View>
           </View>
         </View>
-        <View>
-          <Text style={{ textAlign: "center" }}>
-            Chưa có tài khoản? Đăng ký.
-          </Text>
-        </View>
-      </View>
-    </View>
+      </LinearGradient>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderColor: "red",
-    borderWidth: 5
+    paddingHorizontal: 10
   },
   welcomeText: {
     flex: 0.6,
-    borderColor: "green",
-    borderWidth: 5,
     alignItems: "flex-start",
     justifyContent: "center",
     paddingLeft: 20
   },
   welcomeBtn: {
     flex: 0.4,
-    borderColor: "grey",
-    borderWidth: 5,
     gap: 30
   },
   heading: {
@@ -130,17 +141,7 @@ const styles = StyleSheet.create({
     color: APP_COLOR.ORANGE,
     marginVertical: 10
   },
-  footer: {},
-  btnContainer: {},
-  btnContent: {
-    backgroundColor: "green",
-    padding: 20,
-    borderRadius: 10,
-    alignSelf: "flex-start"
-  },
-  btnText: {
-    textTransform: "uppercase"
-  }
+  footer: {}
 });
 
 export default WelcomePage;
