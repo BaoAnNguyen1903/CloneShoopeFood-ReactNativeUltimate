80134d0234ad71b6187908c9c4b86c5c6865f61c
An Nguyen <anndb1903@gmail.com>
2025-05-20 23:20:50 +0700

    container

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,8 +1,11 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const styles = StyleSheet.create({
-    
-})
+  container: {
+    flex: 1,
+    paddingTop: 30
+  }
+});
 
 const SignUpPage = () => {
   return (
