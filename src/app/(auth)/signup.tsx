d9698fe272cead8278c462e00aca1fd914a9fb79
An Nguyen <anndb1903@gmail.com>
2025-05-20 23:19:11 +0700

    new

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,10 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 const SignUpPage = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <View>
         <Text>Đăng ký tài khoản</Text>
+      </View>
+      <View>
+        <Text>Họ tên</Text>
+        <TextInput />
+      </View>
+      <View>
+        <Text>Email</Text>
+        <TextInput />
       </View>
     </View>
   );
