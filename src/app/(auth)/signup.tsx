5c65afb8c81ed4ddd89cb44666bf376996465fb9
An Nguyen <anndb1903@gmail.com>
2025-05-21 21:10:15 +0700

    safe area view

--- src/app/(auth)/signup.tsx
+++ src/app/(auth)/signup.tsx
@@ -1,11 +1,11 @@
 import ShareButton from "@/components/button/share.button";
 import { APP_COLOR } from "@/utils/constant";
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
     marginHorizontal: 20,
     gap: 10
   },
@@ -27,39 +27,41 @@ const styles = StyleSheet.create({
 
 const SignUpPage = () => {
   return (
-    <View style={styles.container}>
-      <View style={{ padding: 5 }}>
-        <Text>Đăng ký tài khoản</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={{ padding: 5 }}>
+          <Text>Đăng ký tài khoản</Text>
+        </View>
+        <View style={styles.inputGroup}>
+          <Text style={styles.text}>Họ tên</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text style={styles.text}>Email</Text>
+          <TextInput keyboardType="email-address" style={styles.input} />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text style={styles.text}>Password</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <ShareButton
+          title="ĐĂNG KÝ"
+          onPress={() => alert("me")}
+          textStyle={{ color: "#fff", paddingVertical: 5 }}
+          btnStyle={{
+            justifyContent: "center",
+            borderRadius: 30,
+            marginHorizontal: 35,
+            paddingVertical: 15,
+            backgroundColor: APP_COLOR.ORANGE,
+            borderWidth: 1,
+            borderColor: "#505050",
+            width: "82%"
+          }}
+          pressStyle={{ alignSelf: "stretch" }}
+        />
       </View>
-      <View style={styles.inputGroup}>
-        <Text style={styles.text}>Họ tên</Text>
-        <TextInput style={styles.input} />
-      </View>
-      <View style={styles.inputGroup}>
-        <Text style={styles.text}>Email</Text>
-        <TextInput keyboardType="email-address" style={styles.input} />
-      </View>
-      <View style={styles.inputGroup}>
-        <Text style={styles.text}>Password</Text>
-        <TextInput style={styles.input} />
-      </View>
-      <ShareButton
-        title="ĐĂNG KÝ"
-        onPress={() => alert("me")}
-        textStyle={{ color: "#fff", paddingVertical: 5 }}
-        btnStyle={{
-          justifyContent: "center",
-          borderRadius: 30,
-          marginHorizontal: 35,
-          paddingVertical: 15,
-          backgroundColor: APP_COLOR.ORANGE,
-          borderWidth: 1,
-          borderColor: "#505050",
-          width: "82%"
-        }}
-        pressStyle={{ alignSelf: "stretch" }}
-      />
-    </View>
+    </SafeAreaView>
   );
 };
 
