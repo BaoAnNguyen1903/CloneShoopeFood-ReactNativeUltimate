0bf4f6b30b1c7581fa622eb250a0ff2c14bf52d7
An Nguyen <anndb1903@gmail.com>
2025-05-21 22:54:22 +0700

    verify page

---
+++ src/app/(auth)/verify.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const VerifyPage = () => {
+  return (
+    <View>
+      <Text>VerifyPage page</Text>
+    </View>
+  );
+};
+
+export default VerifyPage;
