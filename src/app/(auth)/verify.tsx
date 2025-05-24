41b683e5fb26fd025095845476aecbf79f9fed73
An Nguyen <anndb1903@gmail.com>
2025-05-24 22:29:18 +0700

    input otp

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import OTPTextView from "react-native-otp-textinput";
 
 const VerifyPage = () => {
   return (
     <View>
       <Text>VerifyPage page</Text>
+      <OTPTextView inputCount={5} inputCellLength={2} />
     </View>
   );
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import { RootSiblingParent } from "react-native-root-siblings";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const RootLayout = () => {
   return (
@@ -9,18 +10,21 @@ const RootLayout = () => {
     //   <Text>Footer</Text>
     // </View>
     <RootSiblingParent>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)/signup" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="product/index"
-          options={{ headerTitle: "Sản phẩm" }}
-        />
-        <Stack.Screen
-          name="(auth)/login"
-          options={{ headerTitle: "Đăng nhập" }}
-        />
-      </Stack>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/signup" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)/verify" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="product/index"
+            options={{ headerTitle: "Sản phẩm" }}
+          />
+          <Stack.Screen
+            name="(auth)/login"
+            options={{ headerTitle: "Đăng nhập" }}
+          />
+        </Stack>
+      </SafeAreaView>
     </RootSiblingParent>
   );
 };
