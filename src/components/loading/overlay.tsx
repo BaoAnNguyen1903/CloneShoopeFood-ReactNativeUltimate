a22f6120a2ca425896d3a3847dbc7da9049c1a6f
An Nguyen <anndb1903@gmail.com>
2025-05-24 23:19:52 +0700

    component overlay

--- src/components/loading/overlay.tsx
+++ src/components/loading/overlay.tsx
@@ -1,18 +1,26 @@
-import { StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
-interface IProps {
-
-}
+interface IProps {}
 
 const styles = StyleSheet.create({
-    laoding: {
-        position: "absolute",
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        alignItems: 'center',
-        justifyContent: "center",
-        backgroundColor: "rgba(0,0,0,0.5)"
-    }
-})
+  laoding: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.5)"
+  }
+});
+
+const LoadingOverlay = (props: IProps) => {
+  return (
+    <View style={styles.laoding}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+export default LoadingOverlay;
