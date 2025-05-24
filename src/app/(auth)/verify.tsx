a61703783d3f4e9b698a3d21181e821c6ab43724
An Nguyen <anndb1903@gmail.com>
2025-05-24 23:25:49 +0700

    update

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -1,4 +1,6 @@
+import LoadingOverlay from "@/components/loading/overlay";
 import { APP_COLOR } from "@/utils/constant";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import OTPTextView from "react-native-otp-textinput";
 
@@ -15,33 +17,41 @@ const styles = StyleSheet.create({
 });
 
 const VerifyPage = () => {
+  const [isSubmit, setIsSubmit] = useState<boolean>(false);
+  const hanldeCellTextChange = async (text: string, i: number) => {
+    console.log(">>>check text: ", text, " and i = ", i);
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Xác thực tài khoản</Text>
-      <Text style={{ marginVertical: 10 }}>
-        Vui lòng nhập mã xác nhận đã được gửi tới địa chỉ{" "}
-      </Text>
-      <View style={{ marginVertical: 20 }}>
-        <OTPTextView
-          autoFocus
-          inputCount={6}
-          inputCellLength={1}
-          tintColor={APP_COLOR.ORANGE}
-          textInputStyle={{
-            borderWidth: 1,
-            borderColor: APP_COLOR.GREY,
-            borderBottomWidth: 1,
-            borderRadius: 5,
-            // @ts-ignore: next-line, // djt me ao vl comment code ma` fix dc color
-            color: APP_COLOR.ORANGE
-          }}
-        />
+    <>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Xác thực tài khoản</Text>
+        <Text style={{ marginVertical: 10 }}>
+          Vui lòng nhập mã xác nhận đã được gửi tới địa chỉ{" "}
+        </Text>
+        <View style={{ marginVertical: 20 }}>
+          <OTPTextView
+            handleCellTextChange={hanldeCellTextChange}
+            autoFocus
+            inputCount={6}
+            inputCellLength={1}
+            tintColor={APP_COLOR.ORANGE}
+            textInputStyle={{
+              borderWidth: 1,
+              borderColor: APP_COLOR.GREY,
+              borderBottomWidth: 1,
+              borderRadius: 5,
+              // @ts-ignore: next-line, // djt me ao vl comment code ma` fix dc color
+              color: APP_COLOR.ORANGE
+            }}
+          />
+        </View>
+        <View style={{ flexDirection: "row", marginVertical: 10 }}>
+          <Text>Không nhận được mã xác nhận,</Text>
+          <Text style={{ textDecorationLine: "underline" }}> gửi lại</Text>
+        </View>
       </View>
-      <View style={{ flexDirection: "row", marginVertical: 10 }}>
-        <Text>Không nhận được mã xác nhận,</Text>
-        <Text style={{ textDecorationLine: "underline" }}> gửi lại</Text>
-      </View>
-    </View>
+      {isSubmit && <LoadingOverlay />}
+    </>
   );
 };
 
