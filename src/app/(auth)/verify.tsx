dbddcbfa696360d663c84f9b9c83ea658c3df412
An Nguyen <anndb1903@gmail.com>
2025-05-24 23:04:41 +0700

    update

--- src/app/(auth)/verify.tsx
+++ src/app/(auth)/verify.tsx
@@ -12,23 +12,34 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     marginVertical: 20
   }
-})
+});
 
 const VerifyPage = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Xác thực tài khoản</Text>
-      <OTPTextView
-        inputCount={6}
-        inputCellLength={1}
-        tintColor={APP_COLOR.ORANGE}
-        textInputStyle={{
-          borderWidth: 1,
-          borderColor: APP_COLOR.GREY,
-          borderBottomWidth: 1,
-          borderRadius: 5
-        }}
-      />
+      <Text style={{ marginVertical: 10 }}>
+        Vui lòng nhập mã xác nhận đã được gửi tới địa chỉ{" "}
+      </Text>
+      <View style={{ marginVertical: 20 }}>
+        <OTPTextView
+          inputCount={6}
+          inputCellLength={1}
+          tintColor={APP_COLOR.ORANGE}
+          textInputStyle={{
+            borderWidth: 1,
+            borderColor: APP_COLOR.GREY,
+            borderBottomWidth: 1,
+            borderRadius: 5,
+            // @ts-ignore: next-line, // djt me ao vl comment code ma` fix dc color
+            color: APP_COLOR.ORANGE
+          }}
+        />
+      </View>
+      <View style={{ flexDirection: "row", marginVertical: 10 }}>
+        <Text>Không nhận được mã xác nhận,</Text>
+        <Text style={{ textDecorationLine: "underline" }}> gửi lại</Text>
+      </View>
     </View>
   );
 };
