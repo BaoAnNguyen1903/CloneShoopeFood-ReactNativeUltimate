2b065f1e9f294b2ef8180eda1da1c1cc09899efc
An Nguyen <anndb1903@gmail.com>
2025-05-30 01:43:26 +0700

    css

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,37 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const data = Array(10).fill(1);
 
 const data1 = Array(20).fill(1);
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ecf0f1",
+    flex: 1,
+    justifyContent: "center",
+    overflow: "hidden"
+  },
+  header: {
+    borderColor: "red",
+    borderWidth: 5,
+    height: 100,
+    marginBottom: 6,
+    width: "100%"
+  },
+  item: {
+    borderColor: "green",
+    borderWidth: 5,
+    height: 100,
+    marginBottom: 6,
+    width: "100%"
+  },
+  list: {
+
+  }
+});
+
 const HomeTab = () => {
+  
   return (
     <View>
       <Text>HomeTab page</Text>
