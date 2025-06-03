f81bef6484406802e9c0f025304ea6d69e8438cd
An Nguyen <anndb1903@gmail.com>
2025-06-03 23:32:16 +0700

    config

--- src/utils/axios.customize.ts
+++ src/utils/axios.customize.ts
@@ -14,7 +14,6 @@ const instance = axios.create({
 instance.interceptors.request.use(
   function (config) {
     // Do something before request is sent
-    config.headers["delay"]; // backend suử lý để test
     return config;
   },
   function (error) {
