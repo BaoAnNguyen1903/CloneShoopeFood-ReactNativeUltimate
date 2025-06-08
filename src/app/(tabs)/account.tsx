edac5a218bb4b19ca230fced6772a9f2b5cff2c3
An Nguyen <anndb1903@gmail.com>
2025-06-08 21:49:25 +0700

    design account

--- src/app/(tabs)/account.tsx
+++ src/app/(tabs)/account.tsx
@@ -1,11 +1,56 @@
+import ShareInput from "@/components/input/share.input";
 import { useCurrentApp } from "@/context/app.context";
-import { Text, View } from "react-native";
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    paddingTop: 50
+  }
+});
 
 const AccountTab = () => {
-  const { theme, appState } = useCurrentApp();
+  const { appState } = useCurrentApp();
+  const backend =
+    Platform.OS === "android"
+      ? process.env.EXPO_PUBLIC_ANDROID_API_URL
+      : process.env.EXPO_PUBLIC_IOS_API_URL;
+
+  const baseImage = `${backend}/images/avatar`;
   return (
-    <View>
-      <Text>AccountTab page, appState = {JSON.stringify(appState)}</Text>
+    <View style={styles.container}>
+      <View style={{ alignItems: "center", gap: 5 }}>
+        <Image
+          style={{ height: 150, width: 150 }}
+          source={{ uri: `${baseImage}/${appState?.user.avatar}` }}
+        />
+        <Text>{appState?.user.name}</Text>
+      </View>
+      <View style={{ marginTop: 20, gap: 20 }}>
+        <ShareInput
+          title="Họ tên"
+          // onChangeText={handleChange("name")}
+          // onBlur={handleBlur("name")}
+          value={appState?.user.name}
+          // error={errors.name}
+        />
+
+        <ShareInput
+          title="Email"
+          // onChangeText={handleChange("name")}
+          // onBlur={handleBlur("name")}
+          value={appState?.user.email}
+          // error={errors.name}
+        />
+
+        <ShareInput
+          title="Số điện thoại"
+          // onChangeText={handleChange("name")}
+          // onBlur={handleBlur("name")}
+          value={appState?.user.phone}
+          // error={errors.name}
+        />
+      </View>
     </View>
   );
 };
