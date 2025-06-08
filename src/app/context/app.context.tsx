a0f589e3191d8024943408956fe9b40f944d3c51
An Nguyen <anndb1903@gmail.com>
2025-06-08 18:37:30 +0700

    theme

--- src/app/context/app.context.tsx
+++ src/app/context/app.context.tsx
@@ -2,17 +2,22 @@ import { createContext, useState } from "react";
 
 interface AppContextType {
   theme: string;
+  setTheme: (v: string) => void;
 }
 
-const AppContext = createContext<AppContextType>({ theme: "light" });
+const AppContext = createContext<AppContextType | null>(null);
 
-interface IProps {}
+interface IProps {
+  children: React.ReactNode;
+}
 
 const AppProvider = (props: IProps) => {
-  const [appState, setAppState] = useState();
+  const [theme, setTheme] = useState<string>("");
   return (
-    <AppContext.Provider
-      value={{ appState, setAppState }}
-    ></AppContext.Provider>
+    <AppContext.Provider value={{ theme, setTheme }}>
+      {props.children}
+    </AppContext.Provider>
   );
 };
+
+export default AppProvider;
